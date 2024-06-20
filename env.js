64419cf79fbef4262f01196d5fef2fec3a834d32
Zeal Murapa <zealmurapa@gmail.com>
2024-06-20 23:08:19 +0200

    update

--- env.js
+++ env.js
@@ -2,6 +2,7 @@
 
 const { SecretsManager } = require('@aws-sdk/client-secrets-manager');
 const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3")
+const { STSClient, AssumeRoleCommand } = require('@aws-sdk/client-sts');
 const fs = require('fs')
 
 try {
@@ -17,9 +18,12 @@ require('dotenv').config({ path: process.cwd() + '/.env' })
 const appName = process.argv.splice(2)[0] || process.env.INPUT_SLUG || 'ecs-environment-variables'
 const region = process.env.INPUT_REGION || process.env.AWS_REGION || 'us-east-1'
 
+const awsAccount = process.env.INPUT_AWS_ACCOUNT_ID || process.env.AWS_ACCOUNT_ID
 const secretName = process.env.INPUT_SECRET_NAME || process.env.SECRET_NAME
 const secretFileName = process.env.INPUT_SECRET_FILE_NAME || process.env.SECRET_FILE_NAME
 const bucketName = process.env.INPUT_BUCKET_NAME || process.env.BUCKET_NAME
+const roleSessionName = process.env.INPUT_ROLE_NAME || process.env.ROLE_NAME
+const roleArn = process.env.INPUT_ROLE_ARN || process.env.ROLE_ARN || `arn:aws:iam::${awsAccount}:role/${roleSessionName}`
 
 console.log('###############################################################')
 console.log('APP_SLUG ENV ~ ', appName)
@@ -41,15 +45,34 @@ const awsConfig = {
 
 const client = new SecretsManager(awsConfig)
 
-const s3Client = new S3Client({
-	region,
-	credentials: {
-		accessKeyId: process.env.INPUT_AWS_ACCESS_KEY || process.env.AWS_ACCESS_KEY_ID,
-		secretAccessKey: process.env.INPUT_AWS_SECRET_KEY || process.env.AWS_SECRET_ACCESS_KEY
-	},
-})
+const assumeRole = async () => {
+	const stsClient = new STSClient(awsConfig)	
+	const command = new AssumeRoleCommand({
+	  RoleArn: roleArn,
+	  RoleSessionName: roleSessionName,
+	});
+  
+	const response = await stsClient.send(command)
+	return {
+	  accessKeyId: response.Credentials.AccessKeyId,
+	  secretAccessKey: response.Credentials.SecretAccessKey,
+	  sessionToken: response.Credentials.SessionToken,
+	}
+}
 
 const uploadFileToS3 = async (filePath) => {
+	let credentials
+	if(roleSessionName){
+		credentials = await assumeRole()
+	} else {
+		credentials = awsConfig
+	}
+	
+	const s3Client = new S3Client({
+		region,
+		credentials
+	})
+
 	const fileStream = fs.createReadStream(filePath)
 	const command = new PutObjectCommand({
 		Bucket: bucketName,
